accad5ff700152cd9385f073cc6f1d8701c5ace0
charles <javacs3@gmail.com>
2015-08-30 18:16:26 +0800

    add translator-adapter test

---
+++ test/test.translator-adapter.js
@@ -0,0 +1,33 @@
+'use strict';
+var assert = require('assert');
+var adapter = require('../lib/translator-adapter');
+var translatorStub = {
+  translate: function (word, callback) {
+    callback(null, word);
+  }
+};
+
+describe('translator-adapter', function () {
+  describe('#translate', function () {
+
+    it('callback should be invoked when finished', function (done) {
+      adapter([translatorStub]).translate('word', function () {
+        done();
+      });
+    });
+
+    it('each translator should be invoked when finished', function (done) {
+      var counter = 0;
+      var count = function () {
+        counter++;
+        if (counter === 2) {
+          done();
+        }
+      };
+      adapter([translatorStub, translatorStub]).translate('word', function () {
+        count();
+      });
+    });
+
+  });
+});
