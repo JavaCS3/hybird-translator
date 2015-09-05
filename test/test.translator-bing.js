a388bf053fb138a148d3abd3199e3cc85134fc15
charles <javacs3@gmail.com>
2015-09-05 19:32:28 +0800

    fix bing phonetic bug, and add unit test for it

--- test/test.translator-bing.js
+++ test/test.translator-bing.js
@@ -10,6 +10,7 @@ var bing = require('../lib/translator-bing');
 // response templates
 var normalResponse = fs.readFileSync('test/bing/normal.case');
 var normalResponse2 = fs.readFileSync('test/bing/normal2.case');
+var normalResponse3 = fs.readFileSync('test/bing/normal3.case');
 
 describe('translator-bing', function () {
   describe('#translate (normal case)', function () {
@@ -56,6 +57,47 @@ describe('translator-bing', function () {
   describe('#translate (normal case 2)', function () {
     before(function () {
       mock.stub(request, 'get').yields(null, { statusCode: 200 }, normalResponse2);
+    });
+
+    after(function () {
+      request.get.restore();
+    });
+
+    it('response no error', function () {
+      adapter([bing()]).translate('hello', function (err) {
+        assert.equal(err, null);
+      });
+    });
+
+    it('response has body', function () {
+      adapter([bing()]).translate('hello', function (err, body) {
+        assert.notEqual(body, null);
+        assert.notEqual(body, undefined);
+      });
+    });
+
+    it('response has metadata', function () {
+      adapter([bing()]).translate('hello', function (err, body) {
+        assert.ok(body.hasOwnProperty('metadata'));
+      });
+    });
+
+    it('response has basic definition', function () {
+      adapter([bing()]).translate('hello', function (err, body) {
+        assert.ok(body.hasOwnProperty('basic'));
+        var basic = body.basic;
+        assert.ok(basic.hasOwnProperty('speech'), 'should contain speech');
+        assert.ok(basic.hasOwnProperty('phonetic'), 'should contain phonetic');
+        assert.ok(basic.hasOwnProperty('explains'), 'should contain explains');
+        assert.ok(basic.explains.length > 0);
+      });
+    });
+
+  });
+
+  describe('#translate (normal case 3)', function () {
+    before(function () {
+      mock.stub(request, 'get').yields(null, { statusCode: 200 }, normalResponse3);
     });
 
     after(function () {
