ecb0ea448b5031dafca4d9eebf62bd1830a377eb
charles <javacs3@gmail.com>
2015-09-10 22:46:07 +0800

    add error handlering on view

--- view/script/main.js
+++ view/script/main.js
@@ -1,7 +1,6 @@
 'use strict';
-
-window.$ = window.jQuery = require('./script/jquery-2.1.4.min.js');
-var ejs = require('ejs');
+window.$   = window.jQuery = require('./script/jquery-2.1.4.min.js');
+var ejs    = require('ejs');
 var remote = require('remote');
 
 var template = ejs.compile("<%- include('template/translate-result') -%>", {
@@ -9,7 +8,6 @@ var template = ejs.compile("<%- include('template/translate-result') -%>", {
 });
 
 $(function () {
-
   var translatorAdapter = remote.require('./lib/translator-adapter');
   var bing = remote.require('./lib/translator-bing');
   // var youdao = remote.require('./lib/translator-youdao');
@@ -17,15 +15,15 @@ $(function () {
     result: {}
   };
 
+  // on search event
   $('#search-btn').click(function (e) {
     e.preventDefault();
     var word = $('#search-text').val();
     translatorAdapter([bing()]).translate(word, function (err, body) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      resultSet.result[body.metadata.name] = body;
+      resultSet.result[body.metadata.name] = {
+        error: err,
+        body: body
+      };
       $('#translate-result').html(template(resultSet));
     });
   });
