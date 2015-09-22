0272b4a060b7a386e7697478f3b284dcec28f346
charles <javacs3@gmail.com>
2015-09-22 21:08:14 +0800

    add translator order feature

--- view/js/main.js
+++ view/js/main.js
@@ -9,22 +9,55 @@ var template = ejs.compile("<%- include('template/translate-result') -%>", {
 
 $(function () {
   var translatorAdapter = remote.require('./lib/translator-adapter');
-  var bing = remote.require('./lib/translator-bing');
-  var youdao = remote.require('./lib/translator-youdao');
-  var resultSet = {
-    result: {}
-  };
+  var loader = remote.require('./lib/translator-loader');
+
+  function TranslateResult(m) {
+    var _tmap = (function (m) {
+      var tmap = {};
+      var i;
+      for (i = 0; i < m.length; i++) {
+        tmap[m[i].metadata().name] = i;
+      }
+      return tmap;
+    }(m));
+
+    var _result = (function (m) {
+      var ret = [];
+      var i;
+      for (i = 0; i < m.length; i++) {
+        ret.push({ name: m[i].metadata().name });
+      }
+      return ret;
+    }(m));
+
+    this.setResult = function (translatorName, err, body) {
+      if (Object.keys(_tmap).length === 0) { return; }
+      var index = _tmap[translatorName];
+      _result[index].error = err;
+      _result[index].body = body;
+    };
+
+    this.getResult = function () {
+      return _result;
+    };
+  }
+
+  var translatorModules = [];
+  var result = new TranslateResult(translatorModules);
+
+  loader.load();
+  loader.getTranslatorsAsync(function (t) {
+    translatorModules = t;
+  });
 
   // on search event
   $('#search-btn').click(function (e) {
     e.preventDefault();
     var word = $('#search-text').val();
-    translatorAdapter([bing(), youdao()]).translate(word, function (err, body) {
-      resultSet.result[body.metadata.name] = {
-        error: err,
-        body: body
-      };
-      $('#translate-result').html(template(resultSet));
+    result = new TranslateResult(translatorModules);
+    translatorAdapter(translatorModules).translate(word, function (err, body) {
+      result.setResult(body.metadata.name, err, body);
+      $('#translate-result').html(template({ result: result.getResult() }));
     });
   });
 
